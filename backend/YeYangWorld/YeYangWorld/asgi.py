3d146a9985a541ceeea0d943b069e349f9e37544
yeyangshuo <3391964237@qq.com>
2025-01-18 11:57:14 +0800

    创建django项目

---
+++ backend/YeYangWorld/YeYangWorld/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for YeYangWorld project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YeYangWorld.settings')
+
+application = get_asgi_application()
