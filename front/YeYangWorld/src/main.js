c182450cb2b6e39e02300ec875460a5bd667f281
yeyangshuo <3391964237@qq.com>
2025-01-19 21:50:04 +0800

    添加了axios全局配置以及路由挂载

--- front/YeYangWorld/src/main.js
+++ front/YeYangWorld/src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+import axios from 'axios'
+import router from "./src/router/index";
+
+//创建app
+const app = createApp(App)
+
+//设置全局的axios
+axios.defaults.baseURL = 'http://localhost:8080'
+app.config.globalProperties.$axios = axios
+
+//挂载路由
+app.use(router)
+
+
+
+//挂载app
+app.mount('#app')
