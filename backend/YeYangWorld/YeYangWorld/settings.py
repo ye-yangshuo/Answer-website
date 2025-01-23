054cd2283f9f16a42d94fc935aeeaa915e67793d
yeyangshuo <3391964237@qq.com>
2025-01-23 11:17:16 +0800

    添加了pymysql与mysqlclient库，配置了数据库

--- backend/YeYangWorld/YeYangWorld/settings.py
+++ backend/YeYangWorld/YeYangWorld/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'YeYangWorld.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'common',
+        'USER': 'root',
+        'PASSWORD': '654231',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
