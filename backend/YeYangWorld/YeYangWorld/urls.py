b87dea1e2dfa85a985276636077589cd80d0aaa3
yeyangshuo <3391964237@qq.com>
2025-03-01 19:08:17 +0800

    在userapp中添加了登录验证的api，并配置了url

--- backend/YeYangWorld/YeYangWorld/urls.py
+++ backend/YeYangWorld/YeYangWorld/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from django.contrib import admin
+
+from userApp.views import login_verify
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login_verify/',login_verify, name='login_verify')
+
 ]
--- backend/YeYangWorld/userApp/views.py
+++ backend/YeYangWorld/userApp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from django.http import JsonResponse
+from userApp.models import DtwzUser
 
 # Create your views here.
+@csrf_exempt
+def login_verify(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = DtwzUser.objects.get(name=username, password=password)
+            return JsonResponse({'status': 'success', 'message': '登录成功', 'user_id': user.id})
+        except DtwzUser.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': '用户名或密码无效'})
+    else:
+        return JsonResponse({'status': 'error', 'message': '无效的请求方法'})
