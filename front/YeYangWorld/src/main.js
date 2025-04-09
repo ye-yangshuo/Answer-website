d4947ac6dc658ca7de7a93c59ad6c0418641cedc
yeyangshuo <3391964237@qq.com>
2025-04-09 11:28:53 +0800

    引入了elementplus的组件库，并进行了相关配置

--- front/YeYangWorld/src/main.js
+++ front/YeYangWorld/src/main.js
@@ -4,6 +4,7 @@ import './style.css'
 
 import router from "./router/index";
 import axiosInstance from './utils/GlobalAxios';
+import ElementPlus from 'element-plus';
 
 //创建app
 const app = createApp(App)
@@ -13,6 +14,7 @@ app.provide('axios', axiosInstance)
 
 //挂载路由
 app.use(router)
+app.use(ElementPlus)
 
 //挂载app
 app.mount('#app')
