f528328589ab611f247219e378755ca4293c9011
yeyangshuo <3391964237@qq.com>
2025-04-10 13:20:53 +0800

    构建了导航栏组件

--- front/YeYangWorld/src/components/navigate.vue
+++ front/YeYangWorld/src/components/navigate.vue
@@ -1,16 +1,16 @@
 <template>
-    <el-container class="container">
-      <el-header class="header">Header</el-header>
-      <el-aside width="200px">Aside</el-aside>
-      <el-main>Main</el-main>
-      <el-footer>Footer</el-footer>
-
-    </el-container>
-    
-
-<el-button class='button' type="primary">主要按钮</el-button>
-
-</template>
+  <div class = "top_navigation">
+    <a class="logo" href = 'http://localhost:5173/home'> 
+    </a>
+    <div class="nav-container">
+      <router-link class='nav-item home' to="/home">首页</router-link> 
+      <router-link class='nav-item answer' to="/answer">答题</router-link>
+      <router-link class='nav-item read' to="/read">阅读</router-link>
+      <router-link class='nav-item plan' to="/plan">计划</router-link>
+    </div>
+    <router-link class='my' to="/my"></router-link>
+    </div>
+  </template>
 
 
 <script setup>
@@ -18,17 +18,54 @@
 
 
 <style scoped>
-.container{
+.top_navigation{
   width: 100%;
+  height: 12%;
+  background-color: #ffffff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+}
+.logo{
+  width: 10%;
   height: 100%;
-  background-color: #f5f5f5;
+
+  position: absolute;
+  left:2%;
+
+  background-image: url('../public/image.png');
+  background-size: 60%;
+  background-repeat: no-repeat;
+  background-position: center;
 }
-.header{
-  background-color: #409EFF;
+
+.nav-container {
+  display: flex;
+  justify-content: space-between; 
+  align-items: center; 
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%); 
+  width: 50%; 
 }
-.button{
-  margin-top: 20px;
-  margin-left: 20px;
+.nav-item {
+  font-size: 25px;
+  font-weight: bold;
+  color: #000000;
+  text-decoration: none;
+}
+
+.my {
+  position:absolute;
+  left:92%;
+  width: 7%;
+  height: 100%;
+  background-image: url('../public/personal.png');
+  background-repeat: no-repeat;
+  background-size: 40%;
+  background-position: center;
 }
 
 </style>
