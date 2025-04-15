38f9a70e4f76fe44af89a4ac6aaf8ced6ffee18b
yeyangshuo <3391964237@qq.com>
2025-04-15 14:44:21 +0800

    创建了获取题目的api接口

--- backend/YeYangWorld/YeYangWorld/urls.py
+++ backend/YeYangWorld/YeYangWorld/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from django.contrib import admin
 
 from userApp.views import login_verify , send_code , register_verify, user_verify
+
+from datiApp.views import get_problem
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -28,5 +30,7 @@ urlpatterns = [
 
     path('user/register_verify/',register_verify, name='register_verify'),
 
-    path('user/user_verify/',user_verify, name='user_verify')
+    path('user/user_verify/',user_verify, name='user_verify'),
+
+    path('dati/get_problem/',get_problem, name='get_problem')
 ]
--- backend/YeYangWorld/datiApp/views.py
+++ backend/YeYangWorld/datiApp/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from django.http import JsonResponse
+from datiApp.models import DtwzProblem
+from django.core.serializers import serialize
+import json
+
+from datiApp.models import DtwzProblem,DTWZProblemCompleted
+from myutils import JWT
+jwt = JWT()
+
+@csrf_exempt
+def get_problem(request):
+    if request.method == 'GET':
+        token = request.META.get('HTTP_AUTHORIZATION')
+        token = token.split(' ')[1]
+        userid = jwt.verify_token(token)
+
+        #先根据id查询用户完成表，按照完成id排序，然后根据题目id查询题目表
+        dtwz_problem_completed = DTWZProblemCompleted.objects.filter(user_id=userid)
+        print(dtwz_problem_completed)
+
+        if(dtwz_problem_completed.exists()):
+            #获取完成表中最新的题目id
+            dtwz_problem_completed = dtwz_problem_completed.order_by('-problem_id')
+            max_problem_id = dtwz_problem_completed[0].problem_id
+
+            #获取当前与下一个题目
+            now_problem = DtwzProblem.objects.get(id=max_problem_id+1) 
+            now_problem = serialize('json', [now_problem])
+            now_problem = json.loads(now_problem)
+            now_problem = now_problem[0]['fields']
+
+            next_problem = DtwzProblem.objects.get(id=max_problem_id+2)
+            next_problem = serialize('json', [next_problem])
+            next_problem = json.loads(next_problem)
+            next_problem = next_problem[0]['fields']
+
+            return JsonResponse({'status': 200, 'now_problem': now_problem, 'next_problem': next_problem})
+        else:
+            #获取问题表中最小id与下一个的题目
+            now_problem = DtwzProblem.objects.order_by('id')[0]
+            now_problem = serialize('json', [now_problem])
+            now_problem = json.loads(now_problem)
+            now_problem = now_problem[0]['fields']
+
+            next_problem = DtwzProblem.objects.order_by('id')[1]
+            next_problem = serialize('json', [next_problem])
+            next_problem = json.loads(next_problem)
+            next_problem = next_problem[0]['fields']
+
+            return JsonResponse({'status': 200, 'now_problem': now_problem, 'next_problem': next_problem})
+
+
 
