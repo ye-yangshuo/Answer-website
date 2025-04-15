5f1c99fef96b3007e5db982ab8734bf87c1b10af
yeyangshuo <3391964237@qq.com>
2025-04-15 14:41:15 +0800

    添加了答题完成表，重构了datiapp的迁移历史

--- backend/YeYangWorld/datiApp/models.py
+++ backend/YeYangWorld/datiApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from userApp.models import DtwzUser
 # Create your models here.
 class DtwzProblem(models.Model):
     id = models.AutoField(primary_key=True)
@@ -14,3 +14,20 @@ class DtwzProblem(models.Model):
     class Meta:
         managed = True
         db_table = 'dtwz_problem'
+
+class DTWZProblemCompleted(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(DtwzUser,on_delete=models.CASCADE,db_comment='用户id')
+    problem = models.ForeignKey(DtwzProblem,on_delete=models.CASCADE,db_comment='题目id')
+    istrue = models.BooleanField(db_comment='是否正确')
+    collection = models.BooleanField(db_comment='是否收藏')
+    note = models.TextField(blank=True, null=True, db_comment='用户笔记')
+
+    class Meta:
+        managed = True
+        db_table = 'dtwz_problem_completed'
+        indexes = [
+            models.Index(fields=['user', 'problem'], name='user_problem')
+        ]
+
+
