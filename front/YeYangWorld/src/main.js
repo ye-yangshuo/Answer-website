fd6e9c1ed0f47f5beac4fc81c52a2961f7e5ca60
yeyangshuo <3391964237@qq.com>
2025-04-18 10:45:31 +0800

    完善了elementplus的导入，添加了icon组件的导入

--- front/YeYangWorld/src/main.js
+++ front/YeYangWorld/src/main.js
@@ -5,6 +5,10 @@ import './style.css'
 import router from "./router/index";
 import axiosInstance from './utils/GlobalAxios';
 import ElementPlus from 'element-plus';
+import "element-plus/theme-chalk/index.css";
+import * as icons from '@element-plus/icons-vue';
+import SvgIcon from './components/SvgIcon.vue';
+import './assets/icon/iconfont.js';
 
 //创建app
 const app = createApp(App)
@@ -14,7 +18,15 @@ app.provide('axios', axiosInstance)
 
 //挂载路由
 app.use(router)
+
+//全局挂载element-plus
 app.use(ElementPlus)
+Object.keys(icons).forEach(key => {
+    app.component(key, icons[key])
+  });
+
+//全局挂载svg-icon
+app.component('SvgIcon', SvgIcon)
 
 //挂载app
 app.mount('#app')
