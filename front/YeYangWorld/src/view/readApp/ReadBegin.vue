ba431942620de1696c625f88a9a06a59f6a85a5d
yeyangshuo <3391964237@qq.com>
2025-04-25 13:05:02 +0800

    正式确定了页面布局的结构

--- front/YeYangWorld/src/view/readApp/ReadBegin.vue
+++ front/YeYangWorld/src/view/readApp/ReadBegin.vue
@@ -15,7 +15,6 @@
 
 <style scoped>
 .readbegin {
-    position: relative;
     background-color: #E4E2E2;
     width: 1200px;
     height: 900px;
@@ -23,17 +22,17 @@
     flex-direction: row;
 }
 .left{
-    position: fixed;
+    position: sticky;
     top:70px;
-    width:110px;
-    height: 900px;
+    width:120px;
+    height: 500px;
     background-color: #ffffff;
 }
 .main{
     width:100%;
     height: 900px;
     background-color: #ffffff;
-    margin-left : 120px;
+    margin-left :  10px;
 
 }
 
