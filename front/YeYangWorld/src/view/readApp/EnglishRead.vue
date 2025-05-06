fb38d55c125b0706c739dcede19e2c891bf76bcc
yeyangshuo <3391964237@qq.com>
2025-05-06 23:46:57 +0800

    添加了中英文交替显示，与控制中文显示的功能

--- front/YeYangWorld/src/view/readApp/EnglishRead.vue
+++ front/YeYangWorld/src/view/readApp/EnglishRead.vue
@@ -1,38 +1,105 @@
 <template>
-    <div>
-      <h1>English Read</h1>
-      <div v-html="english" class="article-content"></div>
-        
+  <header class="navigate">
+    <router-link to="/readbegin" class="back">返回</router-link>
+    <a class="logo" href='http://localhost:5173/home'></a>
+  </header>
 
+  <div class="english_read">
+    <div class="content">
+      <div v-for="item in content" :key="item">
+        <span v-html="item.english"></span>
+        <span v-html="item.chinese" v-if="ischinese"></span>
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref, inject ,onBeforeMount} from 'vue';
-  import { useRoute } from 'vue-router';
-  const axios = inject('axios');
-  const route = useRoute();
-  const article_id = route.params.articleid;
+  </div>
 
-  const english = ref();
-  const chinese = ref([]);
+  <div class="tools">
+    <el-button @click="ischinese = !ischinese">{{ ischinese ? '隐藏中文' : '显示中文' }}</el-button>
+  </div>
+
+</template>
+
+<script setup>
+import { ref, inject, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router';
+import separate from '../../utils/Separate';
+const axios = inject('axios');
+const route = useRoute();
+const article_id = route.params.articleid;
+
+const contentstring = ref('');
+const content = ref([]);
+const ischinese = ref(false);
+async function getarticle() {
+  const response = await axios.post('/read/get_article_detail/', { article_id: article_id })
+  const data = response.data.article;
+  contentstring.value = data.content;
+  console.log(contentstring.value);
+  content.value = separate(contentstring.value);
+  console.log(content.value);
+}
+onBeforeMount(getarticle)
 
 
-  async function getarticle(){
-    const response = await axios.post('/read/get_article_detail/', {article_id: article_id})
-    const data = response.data.article;
-    english.value = data.english;
-    chinese.value = data.chinese;
-    console.log(english.value);
-    console.log(chinese.value);
-    console.log(response.data.article);
-  }
-  
-  onBeforeMount(getarticle)
+</script>
 
-  </script>
-  
-  <style scoped>
-  /* 样式内容 */
-  </style>
-  
+<style scoped>
+.navigate {
+  position: sticky;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 45px;
+  display: flex;
+  flex-direction: row;
+  background-color: #ffffff;
+  z-index: 1000;
+  border-bottom: #29313a solid 1px;
+}
+
+.back {
+  display: block;
+  width: 100px;
+  height: 45px;
+}
+
+.logo {
+  width: 100px;
+  height: 45px;
+  position: absolute;
+  left: 45%;
+  background-image: url('/src/assets/userApp/image.png');
+  background-size: 60%;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+
+
+.english_read {
+  width: 700px;
+  background-color: #ffffff;
+  color: #29313a;
+  font-size: 20px;
+  display: flex;
+  flex-direction: column;
+}
+
+.tools {
+  position: sticky;
+  width: 100%;
+  bottom:0%;
+  height: 45px;
+  display: flex;
+  flex-direction: row;
+  background-color: #ffffff;
+  z-index: 1000;
+  border-bottom: #29313a solid 1px;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+
+}
+</style>
