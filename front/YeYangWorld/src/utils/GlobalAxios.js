865ac0062f829ca2823220f0fd2d470d50538c7f
yeyangshuo <3391964237@qq.com>
2025-05-09 10:15:31 +0800

    去除了多余的content头

--- front/YeYangWorld/src/utils/GlobalAxios.js
+++ front/YeYangWorld/src/utils/GlobalAxios.js
@@ -4,8 +4,6 @@ const axiosInstance = axios.create({
   baseURL: 'http://localhost:8000/',
   headers: {
     'Content-Type': 'application/json',
-    'Content-Type' : 'multipart/form-data',
-
   },
   withCredentials: true,
 
