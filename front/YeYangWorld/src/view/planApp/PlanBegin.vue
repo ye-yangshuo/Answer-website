100d7fad4fa024674fcff90aaf134ec563fd42eb
yeyangshuo <3391964237@qq.com>
2025-05-13 10:30:54 +0800

    添加了日期选择器

--- front/YeYangWorld/src/view/planApp/PlanBegin.vue
+++ front/YeYangWorld/src/view/planApp/PlanBegin.vue
@@ -1,11 +1,39 @@
 <template>
-    <navigate></navigate>
-    
+    <navigate class="navigate"></navigate>
+    <div class="planbegin">
+        <div class="left">
+            <Calendar  class='calendar' expanded transparent borderless  title-position="left" />
+        </div>
+        <div class="right">
+
+        </div>
+        </div>
 </template>
 
 <script setup>
+import { Calendar } from 'v-calendar';
+import 'v-calendar/style.css';
 
 </script>
 
 <style scoped>
+.navigate{
+    margin-bottom: 0;
+}
+.planbegin{
+width:1200px;
+height:900px;
+background-color: #ffffff;
+display:flex;
+flex-direction: row;
+}
+.left{
+    width:35%;
+    height:100%;
+    border-right: #383636 solid 1px;
+}
+.calendar{
+
+}
+
 </style>
