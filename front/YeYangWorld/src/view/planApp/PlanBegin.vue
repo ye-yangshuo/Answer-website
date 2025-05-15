15f02f0f0c443d5d00c5cf9b6f89eaa26f6acab3
yeyangshuo <3391964237@qq.com>
2025-05-15 10:56:02 +0800

    添加了(选择并显示当前日期),(回到今天并显示今天日期)的功能

--- front/YeYangWorld/src/view/planApp/PlanBegin.vue
+++ front/YeYangWorld/src/view/planApp/PlanBegin.vue
@@ -2,15 +2,17 @@
     <navigate class="navigate"></navigate>
     <div class="planbegin">
         <div class="left">
-            <Calendar class='calendar' expanded transparent borderless title-position="left" ref="calendar">
-            <template #footer>
-                <div class="w-full px-4 pb-3">
-                    <button class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold w-full px-3 py-1 rounded-md"
-                        @click="moveToday">
-                        Today
-                    </button>
-                </div>
-            </template>
+            <Calendar @dayclick='handleselect' ref="calendar" :attributes="attributes" expanded transparent borderless
+                title-position="left">
+                <template #footer>
+                    <div class="w-full px-4 pb-3">
+                        <button
+                            class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold w-full px-3 py-1 rounded-md"
+                            @click="moveToday">
+                            Today
+                        </button>
+                    </div>
+                </template>
             </Calendar>
         </div>
 
@@ -24,7 +26,7 @@
 
 
             <div class="right_create">
-                <div class="now_time">当前时间</div>
+                <div class="now_time">{{ selectedTime }}</div>
                 <input type="text" class="input" placeholder="请输入计划">
                 <button class="button">创建计划</button>
             </div>
@@ -49,15 +51,40 @@
     </div>
 </template>
 
+
 <script setup>
 import { Calendar } from 'v-calendar';
 import 'v-calendar/style.css';
 import { ref } from 'vue';
 
+const nowDate = ref(new Date());
+const selectedDate = ref(new Date());
 const calendar = ref(null);
+const selectedTime = ref(getTodayDate());
+const attributes = ref([
+    { key: 'today', highlight: { fillMode: 'solid' }, dates: nowDate.value },
+])
 
+//获取特定格式的现在日期
+function getTodayDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+//移动到今天,并显示今天
 function moveToday() {
-  calendar.value.move(new Date());
+    calendar.value.move(nowDate.value);
+    selectedTime.value = getTodayDate();
+    attributes.value[1] = {};
+}
+//选择日期,并显示日期
+function handleselect(value) {
+    // console.log(value)
+    selectedDate.value = new Date(value.id)
+    attributes.value[1] = { key: 'selected', highlight: { fillMode: 'outline' }, dates: selectedDate.value }
+    selectedTime.value = value.id
 }
 
 </script>
