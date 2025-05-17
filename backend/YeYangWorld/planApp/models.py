e97fa53f12fcd73629f2d80ce9976fd2cd4738a0
yeyangshuo <3391964237@qq.com>
2025-05-17 11:58:04 +0800

    在planApp中创建了计划完成表

--- backend/YeYangWorld/planApp/models.py
+++ backend/YeYangWorld/planApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from userApp.models import DtwzUser
+
 
 # Create your models here.
+class PlanCompleted(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(DtwzUser, on_delete=models.CASCADE, db_comment='用户id')
+    content = models.TextField(db_comment='计划内容')
+    create_time = models.DateField(auto_now_add=True, db_comment='创建时间')
+
+    class Meta:
+        managed = True
+        db_table = 'planapp_plan_completed'
+        #创建时间的索引
+        indexes = [
+            models.Index(fields=['user','create_time'],name='user_create_time')
+        ]
+ 
